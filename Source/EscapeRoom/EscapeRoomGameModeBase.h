11e4201bdcaaef3d7a809f00d7e1884c6c943c3b
Justice <adamsjal@uni.edu>
2017-07-01 14:21:46 -0500

    fixed gitignore from being a .txt file

---
+++ Source/EscapeRoom/EscapeRoomGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API AEscapeRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
