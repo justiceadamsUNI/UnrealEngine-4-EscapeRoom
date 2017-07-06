0a44ab475c8fc529810f6021eec4f932d5a9323a
Justice <adamsjal@uni.edu>
2017-07-05 22:35:00 -0500

    updated git ignore and finished initial room design
    Former-commit-id: 8d3175e9eeb9d628afcccaf842408942e3f90305

---
+++ Source/EscapeRoom/OpenDoor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Setting Rotation
+	AActor* Owner = GetOwner();
+	FRotator Rotation = FRotator(0.0f, -65.0f, 0.0f);
+	Owner->SetActorRotation(Rotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
